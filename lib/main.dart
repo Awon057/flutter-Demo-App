14bb231ce651c2080245e379c0b6841d3b2c13cf
Awon <awonzaman@gmail.com>
2020-09-27 01:56:45 +0600

    Code refactor(distributed to different dart file), show toast done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:myapp/randomward.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'api.dart';
 
 void main() {
   runApp(MyApp());
@@ -59,7 +61,7 @@ class _LoginPageState extends State<LoginPage> {
       child: MaterialButton(
         minWidth: MediaQuery.of(context).size.width,
         padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-        onPressed: _startList,
+        onPressed: fetchData,
         child: Text(
           "Login",
           textAlign: TextAlign.center,
@@ -100,103 +102,28 @@ class _LoginPageState extends State<LoginPage> {
     ));
   }
 
+  void fetchData() {
+    fetchAlbum().then((value) {
+      if (value.id == 3)
+        _startList();
+      else {
+        Fluttertoast.showToast(
+            msg: "Wrong user",
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.BOTTOM,
+            timeInSecForIosWeb: 1,
+            backgroundColor: Colors.white,
+            textColor: Colors.blueGrey,
+            fontSize: 16.0);
+        /* Scaffold.of(context).showSnackBar(SnackBar(
+          content: Text("Wrong user"),
+        )); */
+      }
+    });
+  }
+
   void _startList() {
-    Navigator.of(context).push(MaterialPageRoute(
-        builder: (BuildContext context) =>
-            RandomWords() /*{
-       return Scaffold(
-        appBar: AppBar(
-          title: Text('Saved Suggestions'),
-        ),
-        body: RandomWords(),
-      );
-    } */
-        ));
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  @override
-  _RandomWordsState createState() => _RandomWordsState();
-}
-
-class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _saved = Set<WordPair>();
-  final _biggerFont = TextStyle(fontSize: 18.0);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Startup Name Generator'),
-        actions: [IconButton(icon: Icon(Icons.list), onPressed: _pushSaved)],
-      ),
-      body: _buildSuggestions(),
-    );
-  }
-
-  Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: EdgeInsets.fromLTRB(16, 8, 16, 8),
-        itemBuilder: /*1*/ (context, i) {
-          if (i.isOdd) return Divider(); /*2*/
-
-          final index = i ~/ 2; /*3*/
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-          }
-          return _buildRow(_suggestions[index]);
-        });
-  }
-
-  void _pushSaved() {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(builder: (BuildContext context) {
-        final tiles = _saved.map((WordPair pair) {
-          return ListTile(
-            title: Text(
-              pair.asPascalCase,
-              style: _biggerFont,
-            ),
-          );
-        });
-
-        final divided = ListTile.divideTiles(
-          context: context,
-          tiles: tiles,
-        ).toList();
-
-        return Scaffold(
-          appBar: AppBar(
-            title: Text('Saved Suggestions'),
-          ),
-          body: ListView(children: divided),
-        );
-      }),
-    );
-  }
-
-  Widget _buildRow(WordPair pair) {
-    final alreadySaved = _saved.contains(pair);
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-      trailing: Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
-      },
-    );
+    Route route = MaterialPageRoute(builder: (context) => RandomWords());
+    Navigator.pushReplacement(context, route);
   }
 }
